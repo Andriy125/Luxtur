a312df69ff30c18b94cdf659185c642379f13b30
Andriy125m <yaruta.andriy@gmail.com>
2020-03-12 00:45:57 +0200

    patch to zip LUXTURnow

--- js/main.js
+++ js/main.js
@@ -1,38 +1,60 @@
-let arr_obj = [];                                                                           // об єкти з широтою і довготою
-let arr = ['A', 'B', 'C', 'D', 'I', 'F', 'G'];                                              // літери для додавання полів і кружочків
-let index = 2;                                                                              // індекс для масива з літерами                                                                         
+let arr_obj = [];                                                                           // об єкти з широтою і довготою                                                                       
 let add = document.querySelector('.add');                                                   // кнопка додавання полів
 let calculate = document.querySelector('.calculate');                                       // кнопка розрахунку
 let apikey_weather = '3042b9bfd374130956c1e55d218c8156';                                    // ключ для отримання координатів міст
 
 // Get the modal
-let modal = document.getElementById("myModal");
-
+let modal_call = document.getElementById("modal_call");
+let modal_calc = document.getElementById("modal_calculate");
 // Get the button that opens the modal
 let btn = document.getElementById("call");
 
 // Get the <span> element that closes the modal
 let span = document.getElementsByClassName("close")[0];
+$('.all_info__input')[0].checked = false;
+$('.order_remove').css('display', 'none');
+
+let local = new Date();
+let local_date = local.getFullYear() + '-';
+if(local.getMonth() < 10){
+    local_date += '0' + (local.getMonth() + 1) + '-';
+}
+else{
+    local_date += (local.getMonth() + 1) + '-';
+}
+if(local.getDate() < 10){
+    local_date += '0' + local.getDate();
+}
+else{
+    local_date += local.getDate();
+}
+$('.order_date')[0].value = local_date;
+$('.order_date')[0].min = local_date;
+
 
 
 $('.phone').mask('+3 (000) 000 00 00', {placeholder: "Номер телефону"});
 
 // When the user clicks on the button, open the modal
 btn.onclick = function() {
-    modal.style.display = "block";
+    modal_call.style.display = "block";
 }
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
-    modal.style.display = "none";
+    modal_call.style.display = "none";
     ClearCallInputs();
 }
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
+    if (event.target == modal_call) {
+        modal_call.style.display = "none";
+
         ClearCallInputs();
+    }
+    else if(event.target == modal_calc){
+        modal_calc.style.display = "none";
     }
 }
 
@@ -40,61 +62,35 @@ const ClearCallInputs = () => {
     document.querySelectorAll('.call_form__input').forEach(el => {el.value = "";});
 }
 
-$(document).click(function(event) {                                                         // видалення елемента
-    let text = $(event.target).text();
-    let obj = arr.find(el => el === text);
-    if(obj){                                                                    
-        index -= document.querySelectorAll(".circle." + text).length;
-        $( "." + text ).remove();
-        if(index === 0){                // якщо індекс 0, додаємо 1, щоб додавався наступний елемент, так як перший елемент видалити не можна
-            index += 1;
-        }
-    }
-    if(document.querySelectorAll('.circles .circle').length <= 2){
-        $('.form_pasengers').css('padding-right', '20px');
-    }
-});
-
 const addScroll = (elem) => {
     $(elem).css('overflow-y', 'auto');
 }
 
-add.addEventListener('click', () =>{                                                        // додавання полів і кружочків
-    if(document.querySelectorAll('.circles .circle').length >= 2){                                   // якщо елементів 4 і більше, вмикати скролл
-        addScroll('.form_inputs');
-        $('.form_pasengers').css('padding-right', '40px');
+add.addEventListener('click', () =>{                                                        // додавання полів                                              
+    let elements = document.querySelectorAll('.form__input');
+    if(elements.length > 1){
+        addStyles('.fixed_container.calc', '.minus');
     }
-
-    if(arr[index] !== undefined){                                                           // якщо літера існує (не за межами массиву)
-        for(let i = 0; i < arr.length; i++){
-            let indic = false;
-            for(let j = 0; j < document.querySelectorAll('.circle').length; j++){
-                if(document.querySelectorAll('.circle')[j].textContent === arr[i]){
-                    indic = true;
-                    break;
-                }
-            }
-            if(!indic){
-                CreateElement(arr[i], arr[i-1]);
-                return;
-            }
-        }
-    }
-    else{
-        alert('You can\'t do this!');
-    }
+    CreateElement('form__input', elements[elements.length - 1])  
 });
 
-const CreateElement = ( text, after ) => {                                                 // створення полів і кружочків    
-    if(after === 'A'){
-        after = 'requi';
-    }
-    $("<div class='group " + text + "'></div>").insertAfter('.circle.' + after);
-    $("<div class='circle " + text + "' title='Видалити'>" + text + "</div>").insertAfter('.group.' + text);
+const addStyles = (container, button) => {
+    addScroll(container);
+    $(button).css('display', 'inline-flex');
+    $('.crud_buttons').css('margin-top', '15px');
+    $('.crud_buttons').css('justify-content', 'space-around');
+    $('.crud_buttons').css('margin-bottom', '0px');
+}
 
-    $('<label class="form__label ' + text + '">Пункт</label>').insertAfter('.form__input.' + after);
-    $('<input type="text" placeholder="Пункт..." class="form__input ' + text + '"/>').insertAfter('.form__label.' + text);
-    index += 1;
+const removeStyles = (button) => {
+    $(button).css('display', 'none');
+    $('.crud_buttons').css('margin-top', '0px');
+    $('.crud_buttons').css('justify-content', 'center');
+}
+
+const CreateElement = ( _class, after ) => {                                                 // створення полів    
+    $('<input type="text" placeholder="Адреса пункту..." class="' + _class + '" required>')
+    .insertAfter(after);
 }
 
 const Reqursion = (cities, index = 0) => {                                         // рекурсивне отримання і запис координат міст                                                  
@@ -161,8 +157,8 @@ calculate.addEventListener('click', () =>{                                      
                 let text = '';
                 for (let i = 0; i < inputs.length - 1; i++){                            // формування виведення
                  text += 'З ' + inputs[i] + ' дo ' + inputs[i+1] + ' = ' + (response.distances[i][i+1] / 1000).toFixed(0) + ' км <br>';
-                }
-                $('.result').html(text);                                                // виведення результатів
+                }                                                                       // виведення результатів
+                modal_calc.style.display = "block";
             })
         })
         .catch(err => {
@@ -180,17 +176,45 @@ document.querySelector('.order_add').addEventListener('click', (e) => {
     if(addresses.length > 1){
         addScroll('.fixed_container');
         $('.order_remove').css('display', 'inline-flex');
-        $('.order_buttons').css('margin-top', '15px');
+        $('.crud_buttons').css('margin-top', '15px');
     }
-    $('<input type="text" placeholder="Адреса пункту..." class="order_data_input address">')
+    $('<input type="text" placeholder="Адреса пункту..." class="order_data_input address" required>')
     .insertAfter(addresses[addresses.length - 1]);
 })
 
 document.querySelector('.order_remove').addEventListener('click', (e) => {
     let addresses = document.querySelectorAll('.address');
     if(addresses.length < 4){
-        $('.order_remove').css('display', 'none');
-        $('.order_buttons').css('margin-top', '0px');
+        removeStyles('.order_remove');
     }
     addresses[addresses.length - 1].remove();
 })
+
+document.querySelector('.minus').addEventListener('click', (e) => {
+    let inputs = document.querySelectorAll('.form__input');
+    if(inputs.length < 4){
+        removeStyles('.minus');
+    }
+    inputs[inputs.length - 1].remove();
+})
+
+document.querySelector('.all_info').addEventListener('click', (e) => {
+    if($('.all_info__input')[0].checked){
+        $('.results_distances__standard').css('display', 'none');
+        $('.results_distances__all').css('display', 'block');
+    }
+    else
+    {
+        $('.results_distances__standard').css('display', 'block');
+        $('.results_distances__all').css('display', 'none');
+    }
+})
+
+
+// const DistanceSum = (arr) => {
+//     let sum = 0;
+//     for(let i = 0; i < arr.length; i++){
+        
+//     }
+//     return sum;
+// }
