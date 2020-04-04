834b698d7300f5340b924aa53fbc13f8e61f28e6
Your Name <you@example.com>
2020-04-04 11:31:50 +0300

    added swithcing between tabs with changing content

---
+++ js/admin.js
@@ -0,0 +1,36 @@
+$(document).ready(function(){
+    document.querySelector('.menu_icon').addEventListener('click', (e) => {
+        $('.side_menu').toggleClass('active', true);
+    })
+
+    document.querySelector('.close_icon').addEventListener('click', (e) => {
+        $('.side_menu').toggleClass('active', false);
+    })
+
+    document.querySelectorAll('.side_menu__item').forEach((el) => {
+        el.addEventListener('click', (e) => ShowContent(e, `id-${el.getAttribute('id')}`));
+    });
+    $('#main').click();
+});
+
+const ShowContent = (e, name) => {
+    // Declare all variables
+    var tabcontent, tablinks;
+
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (let i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("side_menu__item");
+    for (let i = 0; i < tablinks.length; i++) {
+        $(tablinks[i]).toggleClass('active', false);
+    }
+
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    document.getElementById(name).style.display = "block";
+    $(e.currentTarget).toggleClass("active", true);
+    $('.side_menu').toggleClass('active', false);
+}
