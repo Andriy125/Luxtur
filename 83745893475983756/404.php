26ae1d4a6dd19177f69c78a0d3af73d604e67aee
Your Name <you@example.com>
2020-06-24 12:33:04 +0300

    done 404 page

--- 83745893475983756/404.php
+++ 83745893475983756/404.php
@@ -1,1 +1,21 @@
-<h1>404</h1>
+<!DOCTYPE html>
+<html lang="ua">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>404</title>
+</head>
+<body style="display:flex; justify-content:center; align-items:center; height:85vh; background-color:#cca; padding:0;margin:0">
+    <div class="wrapper" style="display:flex; flex-direction:column; align-items:center; ">
+        <h1 style="font-size:66px; margin-bottom:10px; padding:0">404</h1>
+        <p style="font-size:42px;">Page not found</p>
+        <a href="#" class="link" style="text-decoration:none; font-size:24px; color:#333">Go back</a>
+    </div>
+<script>
+    const link = document.querySelector('.link');
+    link.addEventListener('click', () => {
+        history.back();
+    })
+</script>
+</body>
+</html>
--- 83745893475983756/admin.php
+++ 83745893475983756/admin.php
@@ -6,7 +6,7 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>LUX TUR</title>
+	<title>Admin panel</title>
     <script defer src="./js/all.js"></script>
 	<link rel="stylesheet" href="css/input.css">
     <link rel="stylesheet" href="css/admin.css">
